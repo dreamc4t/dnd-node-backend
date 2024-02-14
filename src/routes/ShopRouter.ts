fe002d77905d30cc08f4acf0c24bbdf281bce742
dreamc4t <eriksund1@gmail.com>
2024-02-14 13:53:57 +0000

    shop router updated

--- src/routes/ShopRouter.ts
+++ src/routes/ShopRouter.ts
@@ -5,7 +5,7 @@ const shopRouter = express.Router()
 
 shopRouter.get('/all', async (req: Request, res: Response) => {
   try {
-    const shops = await Shop.find({}).populate('items')
+    const shops = await Shop.find({})
     res.status(200).json(shops)
   } catch (error) {
     const message = (error as Error).message
@@ -13,24 +13,53 @@ shopRouter.get('/all', async (req: Request, res: Response) => {
   }
 })
 
-//Post Method
-shopRouter.post('/post', (req, res) => {
-  res.send('Post API')
+shopRouter.post('/new', async (req, res) => {
+  try {
+    const newShop = new Shop(req.body)
+    const savedShop = await newShop.save()
+    res.status(201).json(savedShop)
+  } catch (error) {
+    const message = (error as Error).message
+    res.status(500).json({ message })
+  }
 })
 
-//Get by ID Method
-shopRouter.get('/get/:id', (req, res) => {
-  res.send('Get by ID API')
+shopRouter.get('/:id', async (req: Request, res: Response) => {
+  try {
+    const shop = await Shop.findById(req.params.id)
+    if (!shop) {
+      return res.status(404).json({ message: 'Shop not found' })
+    }
+    res.status(200).json(shop)
+  } catch (error) {
+    const message = (error as Error).message
+    res.status(500).json({ message })
+  }
+})
+shopRouter.patch('/update/:id', async (req: Request, res: Response) => {
+  try {
+    const shop = await Shop.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    if (!shop) {
+      return res.status(404).json({ message: 'Shop not found' })
+    }
+    res.status(200).json(shop)
+  } catch (error) {
+    const message = (error as Error).message
+    res.status(500).json({ message })
+  }
 })
 
-//Update by ID Method
-shopRouter.patch('/update/:id', (req, res) => {
-  res.send('Update by ID API')
-})
-
-//Delete by ID Method
-shopRouter.delete('/delete/:id', (req, res) => {
-  res.send('Delete by ID API')
+shopRouter.delete('/delete/:id', async (req: Request, res: Response) => {
+  try {
+    const shop = await Shop.findByIdAndDelete(req.params.id)
+    if (!shop) {
+      return res.status(404).json({ message: 'Shop not found' })
+    }
+    res.status(200).json({ message: 'Shop successfully deleted' })
+  } catch (error) {
+    const message = (error as Error).message
+    res.status(500).json({ message })
+  }
 })
 
 export { shopRouter }
